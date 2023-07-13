df04eba5373a65b2116d6f0bf9e9f168e4d83679
happychap <jroloff22@gmail.com>
2023-07-13 12:35:54 +0200

    station 1 script

---
+++ src/roomOne.ts
@@ -0,0 +1,31 @@
+import { bootstrapExtra } from "@workadventure/scripting-api-extra/dist";
+
+console.log('Script started successfully!');
+
+let currentPopup: any = undefined;
+
+WA.onInit().then(() => {
+    console.log('Scripting API ready');
+    console.log('Player tags: ', WA.player.tags)
+    
+    WA.room.area.onEnter('clock').subscribe(()  => {
+        var anleitung = new String("Anleitung fuer das Bedienen der Station 1");
+
+
+        currentPopup = WA.ui.openPopup("popup", anleitung.toString(), []);
+    })
+
+        WA.room.area.onLeave('clock').subscribe(closePopup)
+
+        bootstrapExtra().then(() => {
+            console.log('Scripting API Extra ready');
+        }).catch(e => console.error(e));
+    }
+}).catch(e => console.error(e));
+
+function closePopup(){
+    if (currentPopup !== undefined) {
+        currentPopup.close();
+        currentPopup = undefined;
+    }
+}
