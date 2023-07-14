da394261471eb7f49b85e5367dabffb7251d70f5
Bach Nguyen <bach.nguyen@stud.htwk-leipzig.de>
2023-07-14 09:20:05 +0200

    del tsx dir

--- tilesets/tsx/TopDownHouse_FloorsAndWalls.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="TopDownHouse_FloorsAndWalls" tilewidth="32" tileheight="32" tilecount="162" columns="18">
- <image source="../TopDownHouse_FloorsAndWalls.png" width="576" height="288"/>
-</tileset>
