bf9d3f9b37e4d5c5dd89b96381c1a6459f9a1eb7
bnguyen2 <bach.nguyen@stud.htwk-leipzig.de>
2023-07-15 11:32:31 +0200

    added area script

--- src/controlMap.ts
+++ src/controlMap.ts
@@ -1,7 +1,9 @@
 /// <reference types="@workadventure/iframe-api-typings" />
 
-import { Popup } from "@workadventure/iframe-api-typings";
 import { bootstrapExtra } from "@workadventure/scripting-api-extra";
+import {getLayersMap, findLayerBoundaries} from '@workadventure/scripting-api-extra';
+import {ITiledMapTileLayer} from "@workadventure/tiled-map-type-guard/dist/ITiledMapTileLayer";
+import {openConfig} from '@workadventure/scripting-api-extra';
 
 console.log('Script started successfully');
 
@@ -15,6 +17,19 @@ var japBellConfig = {
     seek : 0,
     mute : false
 }
+
+const layersAll = await getLayersMap();
+const soundStuffLayer = layersAll.get("sound") as ITiledMapTileLayer;
+
+//const paintArea =  ;
+
+const paintControlArea = WA.room.area.create({
+    name: "paintControlArea",
+    x: 7,
+    y: 18,
+    width: 4,
+    height: 4
+});
 
 // Waiting for the API to be ready
 WA.onInit().then(() => {
@@ -37,14 +52,14 @@ WA.onInit().then(() => {
     //});
 
     let soundBellPopUp;
-
     WA.room.onEnterLayer("soundLever").subscribe(() => {
         soundBellPopUp = WA.ui.openPopup("bellControlPopUp", "Möchtest du die Glocke läutern", [
             {
                 label: "Läuten",
                 className: "success",
-                callback: () => {
+                callback: (popup) => {
                     japBellSound.play(japBellConfig);
+                    popup.close();
                 }
             },
             {
@@ -52,16 +67,27 @@ WA.onInit().then(() => {
                 className: "primary",
                 callback: (popup) => {
                     popup.close();
+                    logBoundariesSoundLayer();
                 }
             }
         ]);
     });
 
     WA.room.onLeaveLayer("soundLever").subscribe(() => {
-        soundBellPopUp.close();
+        //soundBellPopUp.close();
     });
 
     //WA.state.onVariableChange('bgmIsOn')
 }).catch(e => console.error(e));
 
 export {};
+
+function logBoundariesSoundLayer() {
+    const boundaries = findLayerBoundaries(soundStuffLayer);
+    console.log(soundStuffLayer);
+    console.log('Top:' , boundaries.top);
+    console.log('Left:' , boundaries.left);
+    console.log('Bottom:' , boundaries.bottom);
+    console.log('Right:' , boundaries.right);
+}
+
